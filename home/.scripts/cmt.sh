37c9878bc0e93e6ed0167951f6145690de1038ce
Benj <Benjamin.Schwerdtner@gmail.com>
2019-04-22 10:19:24 +0200

    fixed white space

--- home/.scripts/cmt.sh
+++ home/.scripts/cmt.sh
@@ -4,32 +4,32 @@ message=""
 push=0
 addAll=0
 for arg do
-    case ${arg} in 
+    case ${arg} in
         -p|--push)
             push=1
             ;;
         -a)
-			addAll=1
-			;;
-		-ap|-pa)
-			addAll=1
-			push=1
-			;;
-		*)
-            message=${arg}    
+      addAll=1
+      ;;
+    -ap|-pa)
+      addAll=1
+      push=1
+      ;;
+    *)
+            message=${arg}
             ;;
     esac
 
 done
 
 if [ "$addAll" -eq "0" ]; then
-	git commit -m "$message"
-else 
-	git add .
-	git commit -m "$message"
+  git commit -m "$message"
+else
+  git add .
+  git commit -m "$message"
 fi
 
 if [[ ${push} > 0 ]]
 then
-    git push    
+    git push
 fi
