b79c9ab59438c20bc383dde594f3a3864769e13e
Benj <Benjamin.Schwerdtner@gmail.com>
2019-06-08 13:07:00 +0200

    updated unity phone. Still never worked.

--- home/.config/fish/functions/unity_phone.fish
+++ home/.config/fish/functions/unity_phone.fish
@@ -1,3 +1,4 @@
 function unity_phone --description 'forward port for unity profiler'
-    adb -d forward tcp:54999 localabstract:Unity-{com.gamingforgood.clashofstreamers}
+    adb -d forward "tcp:34999" "localabstract:Unity-com.gamingforgood.clashofstreamers"
 end
+
