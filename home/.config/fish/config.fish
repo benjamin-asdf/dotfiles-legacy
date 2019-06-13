7103c66769a2ade62f718a9ec0f8261071a704ba
Benj <Benjamin.Schwerdtner@gmail.com>
2019-06-13 18:12:24 +0200

    Add scripts to fish PATH

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -6,5 +6,6 @@ set -gx VISUAL "emacsclient -a emacs"
 set -gx FZF_DEFAULT_OPTS "--height 40% --layout=reverse --border"
 set -gx FZF_DEFAULT_COMMAND "fd --type f"
 
+set -x PATH $PATH ~/.scripts/
 
 thefuck --alias | source
