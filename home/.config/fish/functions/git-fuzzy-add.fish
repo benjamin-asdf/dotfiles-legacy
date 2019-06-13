187652e92228eb02a1369698c9ff69924b2cb0ec
Benj <Benjamin.Schwerdtner@gmail.com>
2019-06-13 13:17:57 +0200

    Make git fuzzy add work

--- home/.config/fish/functions/git-fuzzy-add.fish
+++ home/.config/fish/functions/git-fuzzy-add.fish
@@ -1,3 +1,3 @@
 function git-fuzzy-add --description 'fuzzy search git status and run git add'
-  git status --porcelain | fzf | awk '{print $2}' | git add
+    git ls-files -m -o --exclude-standard | fzf -m --print0 | xargs -0 -ot git add
 end
