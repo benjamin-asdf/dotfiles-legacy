6989722c5ea794f030e05d9931dc422bb1766057
Benj <Benjamin.Schwerdtner@gmail.com>
2019-06-20 17:53:37 +0200

    Update start idlegame scripts

--- home/.scripts/idlegame-link-extra.sh
+++ home/.scripts/idlegame-link-extra.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env bash
 
-# TODO abstraction (idlegame-link)
-
-assetsMounted=$(mount -l | grep IdleGameSymbolicLink-Extra)
-
-symlink_dir=$COSDIR/IdleGameSymbolicLink-Extra
-
-[ -z "$assetsMounted" ] && sudo mkdir -p $symlink_dir/Assets && sudo mount --bind $IDLEGAMEDIR/Assets $symlink_dir/Assets
-
-spawn-idlegame-konsole.sh idlegame-link $symlink_dir
+start-idlegame-link.sh IdleGameSymbolicLink-Extra
