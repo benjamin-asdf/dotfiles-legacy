96ae4920b06f9208bd4b14f0e432c667ec6e9b12
Benj <Benjamin.Schwerdtner@gmail.com>
2019-07-17 08:48:08 +0200

    Comment out nimble path and add thefuck

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -11,7 +11,9 @@ set -gx COSDIR "/home/benj/idlegame"
 set -gx IDLEGAMEDIR "$COSDIR/IdleGame"
 
 set -x PATH $PATH ~/.scripts/
-set -x PATH $PATH ~/.nimble/bin/
+# set -x PATH $PATH ~/.nimble/bin/
 
 # gtags
 set -gx GTAGSLABEL "pygments"
+
+thefuck --alias | source
