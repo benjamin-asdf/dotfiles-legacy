e657f2282b4bef81b9b744fdbb9feb00298c1a70
Benj <Benjamin.Schwerdtner@gmail.com>
2019-08-29 11:24:31 +0200

    Do some fix to start-idlegame-link

--- home/.scripts/start-idlegame-link.sh
+++ home/.scripts/start-idlegame-link.sh
@@ -4,10 +4,10 @@ link_name=$1
 
 [ -z "$1" ] && echo provide link name && exit 1
 
-assetsMounted=$(mount -l | grep $link_name)
+assetsMounted=$(mount -l | grep "${link_name}/Assets")
 
 symlink_dir=$COSDIR/$link_name
 
-[ -z "$assetsMounted" ] && sudo mkdir -p $symlink_dir/Assets && sudo mount --bind $IDLEGAMEDIR/Assets $symlink_dir/Assets
+[ -z "$assetsMounted" ] && sudo mkdir -p "$symlink_dir"/Assets && sudo mount --bind "$IDLEGAMEDIR"/Assets "$symlink_dir"/Assets
 
-spawn-idlegame-konsole.sh $link_name $symlink_dir
+spawn-idlegame-konsole.sh "$link_name" "$symlink_dir"
