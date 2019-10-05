b448df83a73f8661ec7af37f01cb722dae633419
Benj <Benjamin.Schwerdtner@gmail.com>
2019-10-05 07:39:03 +0200

    Improve some idle game script

--- home/.scripts/spawn-idlegame-konsole.sh
+++ home/.scripts/spawn-idlegame-konsole.sh
@@ -1,7 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/sh
+
 
 unity_args="-forcefree -CacheServerIPAddress 192.168.2.113:80 -buildTarget standalone -projectPath $2"
 
-echo unity args are: $unity_args
+echo unity args are: "$unity_args"
 
-konsole -p tabtitle=$1 -e bash -c "unity-editor $unity_args" &
+konsole -p tabtitle="$1" -e bash -c "unity-editor $unity_args" &
