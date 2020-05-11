bd193fc6e133aca92d9185f04ac0a2b41a2e333a
Benj <Benjamin.Schwerdtner@gmail.com>
2020-05-11 06:23:45 +0200

    Add some prefab scripts

--- home/.local/bin/git-tools/our-prefabs-to-file.sh
+++ home/.local/bin/git-tools/our-prefabs-to-file.sh
@@ -4,4 +4,4 @@ git diff-files --name-only --diff-filter=U -z | grep -z -e '\.prefab$' > conflic
 
 sed -z -e 's|IdleGame/||' conflicted-prefabs | tr "\0" "\n"  > IdleGame/prefabs-for-rewrite.txt
 
-xargs -a conflicted-prefabs -0 -I {} sh -c " git checkout --ours -- {} && git add -- {}"
+xargs -a conflicted-prefabs -0 -I {} sh -c " git checkout --ours -- '{}' || true  && git add -- '{}' || true"
