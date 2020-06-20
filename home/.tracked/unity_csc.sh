3bbbd0f50a03a10ddc7b875b732675a164a1862f
Benj <Benjamin.Schwerdtner@gmail.com>
2020-06-20 17:31:42 +0200

    Update unity csc script

--- home/.tracked/unity_csc.sh
+++ home/.tracked/unity_csc.sh
@@ -3,10 +3,12 @@
 # 1) take the Data/Tools/Roslyn dir from the new unity install
 # /opt/unity/2020.2.0a12/Editor/Data/Tools/Roslyn/ for me and copy it into your repo
 # (could skip step 1 ofcourse)
+# copy to BEST_TOOLS_PATH
 # 2) make a backup of /opt/unity/Unity-2019.2.8f1/Editor/Data/Tools/RoslynScripts/unity_csc.sh this
 # 3) replace it with this script (and fix the paths)
+# if something doesn't work then fix the path man
 
-BEST_TOOLS_PATH=/home/benj/idlegame/IdleGame/Tools/Roslyn/linux
+BEST_TOOLS_PATH=/home/benj/idlegame/Tools/Roslyn/linux
 
 if [ -f "$BEST_TOOLS_PATH/Roslyn/csc" ];
 then
